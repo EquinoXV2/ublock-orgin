39c790b530e80b10351855c174eefc81ba0bc86b
EquinoXV2 <121701145+EquinoXV2@users.noreply.github.com>
2026-04-27 18:50:55 -0500

    Update server.js

--- server.js
+++ server.js
@@ -16,13 +16,26 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Proxy requests to the target URL
+// Proxy requests to the target URL and return content in an about:blank cloaked URL
 app.get('/proxy', (req, res) => {
     const url = req.query.url;
     if (!url) {
         return res.status(400).send('URL parameter is required');
     }
-    proxy.web(req, res, { target: url });
+    proxy.web(req, res, { target: url }, (err, req, res) => {
+        if (err) {
+            return res.status(500).send('Proxy error');
+        }
+        // Modify the response to use about:blank as the URL
+        res.setHeader('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';");
+        res.setHeader('X-Frame-Options', 'ALLOWALL');
+        res.setHeader('X-Content-Type-Options', 'nosniff');
+        res.setHeader('X-XSS-Protection', '0');
+        res.setHeader('Referrer-Policy', 'no-referrer');
+        res.setHeader('Content-Type', 'text/html');
+        res.write(`<html><head><title>Loading...</title></head><body><iframe src="about:blank" style="width:100%;height:100%;border:none;"></iframe><script>document.querySelector('iframe').contentWindow.location.replace('${url}');</script></body></html>`);
+        res.end();
+    });
 });
 
 // Start the server
