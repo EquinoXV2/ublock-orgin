dec485ab950fd980bd5d1df2343c442f37ba0d5a
EquinoXV2 <121701145+EquinoXV2@users.noreply.github.com>
2026-04-27 18:46:26 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,15 @@ dotenv.config();
 const app = express();
 const proxy = httpProxy.createProxyServer({});
 
+// Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Serve index.html for the root URL
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Proxy requests to the target URL
 app.get('/proxy', (req, res) => {
     const url = req.query.url;
     if (!url) {
@@ -18,6 +25,7 @@ app.get('/proxy', (req, res) => {
     proxy.web(req, res, { target: url });
 });
 
+// Start the server
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running on port ${process.env.PORT || 3000}`);
 });
